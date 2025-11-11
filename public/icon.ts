69acfe9b6c72dea7b9043ea9ca558c522a6d40d9
guimsegantini-cmd <gui.msegantini@gmail.com>
2025-11-11 14:40:48 -0300

    Create icon.ts

---
+++ public/icon.ts
@@ -0,0 +1,7 @@
+<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 24 24">
+  <rect width="24" height="24" rx="4" fill="#fb923c"/>
+  <g stroke="#ffffff" fill="none" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" transform="translate(0, -1)">
+    <rect x="2" y="7" width="20" height="14" rx="2" ry="2"></rect>
+    <path d="M16 21V5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16"></path>
+  </g>
+</svg>
